f1b89b861a9fe02fc669219fd8c27a749a7de136
katsuo.nishibori <katsuo.nishibori@gmail.com>
2023-09-12 22:42:22 +0900

    init

---
+++ src/main/java/dev/borikatsu/dddsample/DddSampleApplication.java
@@ -0,0 +1,13 @@
+package dev.borikatsu.dddsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DddSampleApplication.class, args);
+  }
+
+}
